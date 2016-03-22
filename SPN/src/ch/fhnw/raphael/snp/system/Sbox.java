18dc31d9129ca7053cde863a09f503b769e9a733
Raphael Brunner <raphi.brunner@me.com>
2016-03-22 14:08:35 +0100

    implemented sbox invert and startet sbox use implementation

--- SPN/src/ch/fhnw/raphael/snp/system/Sbox.java
+++ SPN/src/ch/fhnw/raphael/snp/system/Sbox.java
@@ -1,5 +1,6 @@
 package ch.fhnw.raphael.snp.system;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Sbox {
@@ -19,12 +20,24 @@ public class Sbox {
 	}
 	
 	public byte[] use(byte[] input){
-		//TODO implement box using
-		return input;
+		return boxing(input, box);
 	}
 	public byte[] useInvers(byte[] input){
-		//TODO implement box using invers
+		return boxing(input, inverse());
+	}
+	private byte[] boxing(byte[] input, Map<Byte, Byte> box){
+		//TODO implement box
+		byte bm1 = (byte)0xf0;
+		byte bm2 = (byte)0x0f;
+		
 		return input;
+	}
+	private Map<Byte, Byte> inverse(){
+		Map<Byte, Byte> inverse = new HashMap<Byte, Byte>();
+		for(Byte b: box.keySet()){
+			inverse.put(box.get(b), b);
+		}
+		return inverse;
 	}
 
 }
