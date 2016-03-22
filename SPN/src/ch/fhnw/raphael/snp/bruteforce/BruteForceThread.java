123c312b1b3ca9fdfb1284248b7d5aef0783bba7
Raphael Brunner <raphi.brunner@me.com>
2016-03-22 20:05:52 +0100

    implemented bruteforce

--- SPN/src/ch/fhnw/raphael/snp/bruteforce/BruteForceThread.java
+++ SPN/src/ch/fhnw/raphael/snp/bruteforce/BruteForceThread.java
@@ -22,18 +22,20 @@ public class BruteForceThread extends Thread{
 		int round = 0;
 		while (round < times){
 			boolean foundkey = true;
+			//check all known chiffretext if we find maching key
 			for(Byte[] b: test.keySet()){
 				byte[] bt =  new byte[b.length];
 				for(int i = 0; i < bt.length; i++){
 					bt[i] = b[i];
 				}
 				for(int i = 0; i < bt.length; i++){
-					
+					//Check if we found a matching key
 					if(bt[i] != snp.decode(bt)[i]){
 						foundkey = false;
 						break;
 					}
 				}
+				//if no key match, we don't need to check for the others
 				if(!foundkey) break;
 			}
 			if(foundkey) list.add(snp.getKey());
